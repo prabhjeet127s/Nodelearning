24555bffebc92bcf9169f8333d565d4745833e5b
Drmac <prabhjeet127.s@gmail.com>
2026-07-13 12:36:58 +0530

    first push

---
+++ day 1/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About Page')
+})
+
+app.get('/profile', (req, res) => {
+  res.send('<h1>Profile Page</h1>')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening onnjbfjf port ${port}`)
+})
